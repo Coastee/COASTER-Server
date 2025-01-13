3270761ab7ce8f1275f71f263a208d07d5f14a1e
Lucy Oh <56earls@gmail.com>
2025-01-14 01:01:34 +0900

    Test: 유저 프로필 조회 테스트 #6

---
+++ src/test/java/com/coastee/server/ServiceTest.java
@@ -0,0 +1,10 @@
+package com.coastee.server;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public abstract class ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
---
+++ src/test/java/com/coastee/server/user/service/UserServiceTest.java
@@ -0,0 +1,54 @@
+package com.coastee.server.user.service;
+
+import com.coastee.server.ServiceTest;
+import com.coastee.server.fixture.UserFixture;
+import com.coastee.server.global.apipayload.exception.GeneralException;
+import com.coastee.server.user.domain.User;
+import com.coastee.server.user.domain.repository.UserRepository;
+import com.coastee.server.user.dto.response.UserProfileResponse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static com.coastee.server.global.apipayload.code.status.ErrorStatus.INVALID_USER_ID;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Transactional
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+class UserServiceTest extends ServiceTest {
+
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = userRepository.save(UserFixture.get());
+    }
+
+    @DisplayName("[프로필 조회] 회원의 프로필이 정상적으로 조회된다.")
+    @Test
+    void getProfile() throws Exception{
+        // when
+        UserProfileResponse actual = userService.getProfile(user.getId());
+
+        // then
+        assertThat(actual).usingRecursiveComparison().isEqualTo(UserProfileResponse.from(user));
+    }
+
+    @DisplayName("[프로필 조회] 유효하지 않은 아이디일시, 에러가 발생한다.")
+    @Test
+    void getProfile_withInvalidId() throws Exception{
+        // when & then
+        GeneralException e = assertThrows(GeneralException.class, () -> userService.getProfile(0L));
+        assertEquals(INVALID_USER_ID, e.getCode());
+    }
+}
