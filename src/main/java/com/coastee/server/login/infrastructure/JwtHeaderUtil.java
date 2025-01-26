2a1bda5ff7b6289ae034a12f781dec1b6e12c7ab
Lucy Oh <56earls@gmail.com>
2025-01-26 21:51:31 +0900

    Feat: stomp token validation 과정 추가 #4

--- src/main/java/com/coastee/server/login/infrastructure/JwtHeaderUtil.java
+++ src/main/java/com/coastee/server/login/infrastructure/JwtHeaderUtil.java
@@ -5,32 +5,19 @@ import com.coastee.server.global.apipayload.exception.handler.InvalidJwtExceptio
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.util.StringUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import static com.coastee.server.global.Constant.*;
+import static com.coastee.server.global.Constant.TOKEN_PREFIX;
 import static com.coastee.server.global.apipayload.code.status.ErrorStatus.FAIL_VALIDATE_TOKEN;
 import static com.coastee.server.global.apipayload.code.status.ErrorStatus.NULL_TOKEN;
 
 public class JwtHeaderUtil {
 
-    public static String getAccessToken() {
-        return getToken(HEADER_AUTHORIZATION);
-    }
-
-    public static String getAccessToken(final StompHeaderAccessor accessor) {
-        return accessor.getFirstNativeHeader(HEADER_AUTHORIZATION);
-    }
-
-    public static String getRefreshToken() {
-        return getToken(HEADER_REFRESH_TOKEN);
-    }
-
-    public static String getToken(final String tokenHeader) {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-                .currentRequestAttributes())
-                .getRequest();
-        return getToken(tokenHeader, request);
+    public static String getToken(
+            final String tokenHeader,
+            final StompHeaderAccessor accessor
+    ) {
+        String headerValue = accessor.getFirstNativeHeader(tokenHeader);
+        return extractToken(headerValue);
     }
 
     public static String getToken(
@@ -38,6 +25,10 @@ public class JwtHeaderUtil {
             final HttpServletRequest request
     ) {
         String headerValue = request.getHeader(tokenHeader);
+        return extractToken(headerValue);
+    }
+
+    private static String extractToken(final String headerValue) {
         if (headerValue == null || headerValue.isEmpty()) throw new GeneralException(NULL_TOKEN);
         if (StringUtils.hasText(headerValue) && headerValue.startsWith(TOKEN_PREFIX)) {
             return headerValue.substring(TOKEN_PREFIX.length());
