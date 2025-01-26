8bdc317cc9ce47368d9dbe2b35634b56504b27e7
Lucy Oh <56earls@gmail.com>
2025-01-27 04:37:52 +0900

    Docs: 서버 탈퇴 기능 #40

--- src/test/java/com/coastee/server/server/controller/ServerControllerTest.java
+++ src/test/java/com/coastee/server/server/controller/ServerControllerTest.java
@@ -20,6 +20,8 @@ import static org.springframework.restdocs.headers.HeaderDocumentation.requestHe
 import static org.springframework.restdocs.payload.JsonFieldType.*;
 import static org.springframework.restdocs.payload.JsonFieldType.STRING;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.restdocs.restassured.RestAssuredRestDocumentation.document;
 
 @DisplayName("서버 컨트롤러 테스트")
@@ -29,6 +31,36 @@ class ServerControllerTest extends ControllerTest {
 
     @MockitoBean
     private ServerFacade serverFacade;
+    
+    @DisplayName("서버에서 탈퇴한다.")
+    @Test
+    void exit() throws Exception{
+        // given
+        doNothing().when(serverFacade).enter(any(), any());
+        ServerEntryRequest request = new ServerEntryRequest(List.of(1L, 2L, 3L));
+
+        // when & then
+        RestAssured.given(spec).log().all()
+                .header(ACCESS_TOKEN_HEADER, ACCESS_TOKEN)
+                .body(request)
+                .contentType(ContentType.JSON)
+                .filter(
+                        document("exit-server",
+                                pathParameters(
+                                    parameterWithName("serverId").description("서버 아이디")
+                                ),
+                                requestHeaders(
+                                        headerWithName(ACCESS_TOKEN_HEADER).description("액세스 토큰")
+                                ),
+                                responseFields(
+                                        fieldWithPath("isSuccess").type(BOOLEAN).description("성공 여부"),
+                                        fieldWithPath("code").type(STRING).description("결과 코드"),
+                                        fieldWithPath("message").type(STRING).description("결과 메세지")
+                                )
+                        ))
+                .when().delete("/api/v1/servers/{serverId}", 1)
+                .then().log().all().statusCode(200);
+    }
 
     @DisplayName("서버에 참여하다.")
     @Test
