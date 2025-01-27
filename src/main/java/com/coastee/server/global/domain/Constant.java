ff635d6b15c1ffbe58b7f836db43336b0ef5e4cd
Lucy Oh <56earls@gmail.com>
2025-01-28 00:12:41 +0900

    Fix: header validation 과정 추가 #4

--- src/main/java/com/coastee/server/global/domain/Constant.java
+++ src/main/java/com/coastee/server/global/domain/Constant.java
@@ -3,6 +3,7 @@ package com.coastee.server.global.domain;
 public class Constant {
     // auth
     public static final String TOKEN_PREFIX = "Bearer ";
+    public static final int TOKEN_PREFIX_LENGTH = 7;
     public static final String HEADER_AUTHORIZATION = "Authorization";
     public static final String HEADER_REFRESH_TOKEN = "RefreshToken";
     public static final String AUTHORITIES_KEY = "role";
