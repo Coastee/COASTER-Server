cd4513d3c4556f10fe4f4ebc307f75f3a7c22ba5
Lucy Oh <56earls@gmail.com>
2025-02-19 03:33:34 +0900

    Fix: limit 대신 다른방식으로 변경 #57

--- src/main/java/com/coastee/server/dmroom/domain/repository/DMRoomCustomRepository.java
+++ src/main/java/com/coastee/server/dmroom/domain/repository/DMRoomCustomRepository.java
@@ -13,20 +13,18 @@ public interface DMRoomCustomRepository extends JpaRepository<DirectMessageRoom,
 
     @Query("""
             select new com.coastee.server.dmroom.domain.repository.dto.FindAnotherUserAndRecentDM(
-                r.id, e1.user, d
+                r.id, u, d
             ) from DirectMessageRoom r
             join DirectMessageRoomEntry e1
                 on r = e1.directMessageRoom
                 and e1.user != :user
                 and e1.status = 'ACTIVE'
                 and r in :dmRoomList
-            join fetch e1.user
+            join User u on u = e1.user
             left join DirectMessage d
                 on r = d.directMessageRoom
-                and d.id = (select d2.id from DirectMessage d2
-                            where r = d2.directMessageRoom
-                            order by d2.createdDate desc limit 1)
-            where (select count(e2) from DirectMessageRoomEntry e2 where e2.directMessageRoom = d and e2.status = 'ACTIVE') = 2
+                and d.createdDate = (select max(d2.createdDate) from DirectMessage d2 where r = d2.directMessageRoom)
+            where (select count(e2) from DirectMessageRoomEntry e2 where r = e2.directMessageRoom and e2.status = 'ACTIVE') = 2
             """)
     List<FindAnotherUserAndRecentDM> findAnotherUserAndDMByDMRoomListAndUser(
             @Param("dmRoomList") final List<DirectMessageRoom> dmRoomList,
