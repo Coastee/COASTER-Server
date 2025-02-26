dffb6a41cdf3634d2f102bc815af316fd888307d
Lucy Oh <56earls@gmail.com>
2025-02-27 01:49:59 +0900

    Docs: 소셜 로그인 문서 삭제 #72

--- src/test/java/com/coastee/server/login/controller/LoginControllerTest.java
+++ src/test/java/com/coastee/server/login/controller/LoginControllerTest.java
@@ -31,43 +31,6 @@ class LoginControllerTest extends ControllerTest {
 
     }
 
-    @DisplayName("소셜 로그인을 한다.")
-    @Test
-    void login() throws Exception {
-        // given
-        when(loginFacade.login(any()))
-                .thenReturn(
-                        AuthTokens.of()
-                                .accessToken(ACCESS_TOKEN)
-                                .refreshToken(REFRESH_TOKEN)
-                                .subject(currentUser.getId().toString())
-                                .build()
-                );
-
-        // when & then
-        RestAssured.given(spec).log().all()
-                .param("code", "12345")
-                .contentType(ContentType.JSON)
-                .filter(
-                        document("login",
-                                queryParameters(
-                                        parameterWithName("code").description("callback 결과값 코드")
-                                ),
-                                responseFields(
-                                        fieldWithPath("isSuccess").type(BOOLEAN).description("성공 여부"),
-                                        fieldWithPath("code").type(STRING).description("결과 코드"),
-                                        fieldWithPath("message").type(STRING).description("결과 메세지"),
-                                        fieldWithPath("result").type(OBJECT).description("결과 데이터"),
-                                        fieldWithPath("result.tokenType").type(STRING).description("토큰 타입: Bearer로 고정"),
-                                        fieldWithPath("result.userId").type(NUMBER).description("유저 아이디"),
-                                        fieldWithPath("result.accessToken").type(STRING).description("액세스 토큰"),
-                                        fieldWithPath("result.refreshToken").type(STRING).description("리프레시 토큰")
-                                )
-                        ))
-                .when().get("/api/v1/login/naver-callback")
-                .then().log().all().statusCode(200);
-    }
-
     @DisplayName("토큰을 재발급한다.")
     @Test
     void refreshToken() throws Exception {
