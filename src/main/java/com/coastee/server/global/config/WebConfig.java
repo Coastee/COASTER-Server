081d83adce33697ff6a1a6ad31f8af602273cb2f
Lucy Oh <56earls@gmail.com>
2025-03-04 23:38:56 +0900

    Add: cors config #72

--- src/main/java/com/coastee/server/global/config/WebConfig.java
+++ src/main/java/com/coastee/server/global/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.coastee.server.global.config;
 import com.coastee.server.chatroom.util.ChatRoomTypeConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(final FormatterRegistry registry) {
         registry.addConverter(new ChatRoomTypeConverter());
+    }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://coasterchat.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
+                .allowCredentials(true);
     }
 }
